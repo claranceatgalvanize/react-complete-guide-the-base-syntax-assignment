4663a271387b472ff08b2378977e8c699fe6cea9
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-10-28 17:32:35 -0700

    added state to the app component and sent it down to be used in the userOutput compnent

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import UserInput from "./components/UserInput";
 import UserOutput from "./components/UserOutput";
 
 class App extends Component {
+  state = {
+    username: "Tom"
+  };
   render() {
     return (
       <div className="App">
         <h2>Reat Base Syntax</h2>
         <UserInput />
-        <UserOutput username="Bob" />
+        <UserOutput username={this.state.username} />
       </div>
     );
   }
